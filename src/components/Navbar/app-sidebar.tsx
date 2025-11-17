4a88a6ffa2e6c0ca772faa7efb95b94d2cf98726
“MITCHYUGAN” <denmichigan123@gmail.com>
2025-11-17 09:26:37 +0100

    feat: Worked on Wallet connect, Implemented Wagmi and Rainbow and updated the theme

--- src/components/Navbar/app-sidebar.tsx
+++ src/components/Navbar/app-sidebar.tsx
@@ -14,9 +14,12 @@ import {
 import { Bell, Bookmark, ChevronUp, Home, Mail, MessageSquare, Newspaper, PlusCircle, Search, Settings, User2, Video, X } from "lucide-react";
 import { InputGroup, InputGroupAddon, InputGroupInput } from "../ui/input-group";
 import { Button } from "../ui/button";
-import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuTrigger } from "@radix-ui/react-dropdown-menu";
+// import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuTrigger } from "@radix-ui/react-dropdown-menu";
 import { ModeToggle } from "../../features/Dark_LightMode/mode-toggle";
 import { useTheme } from "@/features/Dark_LightMode/theme-provider";
+import { ConnectButton } from "@rainbow-me/rainbowkit";
+import { useAccount, useDisconnect } from "wagmi";
+import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuTrigger } from "../ui/dropdown-menu";
 
 const browseitems = [
   {
@@ -43,6 +46,11 @@ const browseitems = [
 
 const moreitems = [
   {
+    title: "Profile",
+    url: "#",
+    icon: User2,
+  },
+  {
     title: "Bookmarks",
     url: "#",
     icon: Bookmark,
@@ -67,49 +75,50 @@ const moreitems = [
 export function AppSidebar() {
   const { theme } = useTheme();
   const { toggleSidebar } = useSidebar();
+  const { address } = useAccount();
+  const { disconnect } = useDisconnect();
+
+  const margin = address ? "0px" : "100px"
+  console.log("Margin", margin)
 
   return (
-      <Sidebar>
-        <SidebarHeader>
-          <X className="md:hidden absolute right-5" onClick={() => toggleSidebar()} />
-          <div className="flex flex-col items-start mt-5 gap-5">
-            <a href="/home">
-              <div className="sidebar_logo">
-                {theme === "light" ? <img className="w-50 " src="src/assets/irysocial_logo_dark.png" alt="" /> : <img className="w-50" src="src/assets/irysocial_logo.png" alt="" />}
-              </div>
-            </a>
-            <InputGroup>
-              <InputGroupInput placeholder="Search..." />
-              <InputGroupAddon>
-                <Search />
-              </InputGroupAddon>
-            </InputGroup>
-          </div>
-        </SidebarHeader>
-        <SidebarContent>
-          <SidebarGroup>
-            <SidebarGroupLabel>Browse</SidebarGroupLabel>
-            <SidebarGroupContent>
-              <SidebarMenu>
-                {browseitems.map((item) => (
-                  <SidebarMenuItem key={item.title}>
-                    <SidebarMenuButton asChild>
-                      <a href={item.url}>
-                        <item.icon />
-                        <span>{item.title}</span>
-                      </a>
-                    </SidebarMenuButton>
-                  </SidebarMenuItem>
-                ))}
-              </SidebarMenu>
-            </SidebarGroupContent>
-          </SidebarGroup>
-          <SidebarGroup>
-            <Button className="button">
-              {" "}
-              <PlusCircle /> Create
-            </Button>
-          </SidebarGroup>
+    <Sidebar>
+      <SidebarHeader>
+        <X className="md:hidden absolute right-5" onClick={() => toggleSidebar()} />
+        <div className="flex flex-col items-start mt-5 gap-5">
+          <a href="/home">
+            <div className="sidebar_logo">
+              {theme === "light" ? <img className="w-50 " src="src/assets/irysocial_logo_dark.png" alt="" /> : <img className="w-50" src="src/assets/irysocial_logo.png" alt="" />}
+            </div>
+          </a>
+          <InputGroup>
+            <InputGroupInput placeholder="Search..." />
+            <InputGroupAddon>
+              <Search />
+            </InputGroupAddon>
+          </InputGroup>
+        </div>
+      </SidebarHeader>
+      <SidebarContent>
+        <SidebarGroup>
+          <SidebarGroupLabel>Browse</SidebarGroupLabel>
+          <SidebarGroupContent>
+            <SidebarMenu>
+              {browseitems.map((item) => (
+                <SidebarMenuItem key={item.title}>
+                  <SidebarMenuButton asChild>
+                    <a href={item.url}>
+                      <item.icon />
+                      <span>{item.title}</span>
+                    </a>
+                  </SidebarMenuButton>
+                </SidebarMenuItem>
+              ))}
+            </SidebarMenu>
+          </SidebarGroupContent>
+        </SidebarGroup>
+
+        {address && (
           <SidebarGroup>
             <SidebarGroupLabel>More</SidebarGroupLabel>
             <SidebarGroupContent>
@@ -127,10 +136,23 @@ export function AppSidebar() {
               </SidebarMenu>
             </SidebarGroupContent>
           </SidebarGroup>
-        </SidebarContent>
-        <SidebarFooter>
-          <div className="flex items-center gap-3">
-            <ModeToggle />
+        )}
+
+        {address && (
+          <SidebarGroup>
+            <Button className="button">
+              {" "}
+              <PlusCircle /> Create
+            </Button>
+          </SidebarGroup>
+        )}
+        
+      </SidebarContent>
+      <SidebarFooter className="flex flex-col items-center justify-center">
+        <div className={`flex items-center gap-3 mb-[${margin}] `}>
+          {!address ? (
+            <ConnectButton />
+          ) : (
             <SidebarMenu>
               <SidebarMenuItem>
                 <DropdownMenu>
@@ -140,22 +162,18 @@ export function AppSidebar() {
                       <ChevronUp className="ml-auto" />
                     </SidebarMenuButton>
                   </DropdownMenuTrigger>
-                  <DropdownMenuContent>
+                  <DropdownMenuContent side="top" className="w-[--radix-popper-anchor-width]">
                     <DropdownMenuItem>
-                      <span>Account</span>
-                    </DropdownMenuItem>
-                    <DropdownMenuItem>
-                      <span>Billing</span>
-                    </DropdownMenuItem>
-                    <DropdownMenuItem>
-                      <span>Sign out</span>
+                      <Button className="cursor-pointer" variant={"destructive"} onClick={() => disconnect()}>Sign out</Button>
                     </DropdownMenuItem>
                   </DropdownMenuContent>
                 </DropdownMenu>
               </SidebarMenuItem>
             </SidebarMenu>
-          </div>
-        </SidebarFooter>
-      </Sidebar>
+          )}
+          {/* <ModeToggle /> */}
+        </div>
+      </SidebarFooter>
+    </Sidebar>
   );
 }
--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,30 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
 import { ThemeProvider } from "./features/Dark_LightMode/theme-provider.tsx";
+import { WagmiProvider } from "wagmi";
+import { config } from "./lib/wagmi.ts";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { darkTheme, RainbowKitProvider } from "@rainbow-me/rainbowkit";
+import "@rainbow-me/rainbowkit/styles.css";
+
+const queryClient = new QueryClient();
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
-      <App />
+      <WagmiProvider config={config}>
+        <QueryClientProvider client={queryClient}>
+          <RainbowKitProvider
+            theme={darkTheme({
+              accentColor: "oklch(0.9019 0.152 173.5161)",
+              accentColorForeground: "black",
+              borderRadius: "small",
+            })}
+          >
+            <App />
+          </RainbowKitProvider>
+        </QueryClientProvider>
+      </WagmiProvider>
     </ThemeProvider>
   </StrictMode>
 );
